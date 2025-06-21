374cbaa2a907f00fdda19d1f622b74b3dd5a5bbd
Alin <eternalsq1337@gmail.com>
2025-06-21 20:09:42 +0300

    changed time zone of the app

--- src/main/java/com/example/YumDash/YumDashApplication.java
+++ src/main/java/com/example/YumDash/YumDashApplication.java
@@ -3,10 +3,13 @@ package com.example.YumDash;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class YumDashApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Bucharest"));
 		SpringApplication.run(YumDashApplication.class, args);
 	}
 
